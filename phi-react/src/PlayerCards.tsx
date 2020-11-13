3129380e727fdf9438a4e5b2cb1fffa10aa6ec06
Owen Nelson <onelson@gmail.com>
2020-11-12 21:28:45 -0800

    Throw confetti if we reach consensus.

--- phi-react/src/PlayerCards.tsx
+++ phi-react/src/PlayerCards.tsx
@@ -1,5 +1,20 @@
 import React from 'react';
 import { GetGameState } from './__generated__/GetGameState';
+import Confetti from 'react-dom-confetti';
+
+const confettiConfig = {
+  angle: 90,
+  spread: 360,
+  startVelocity: 40,
+  elementCount: 200,
+  dragFriction: 0.12,
+  duration: 4_000,
+  stagger: 3,
+  width: '10px',
+  height: '10px',
+  perspective: '500px',
+  colors: ['#a864fd', '#29cdff', '#78ff44', '#ff718d', '#fdff6a'],
+};
 
 type Props = {
   // should be fine to receive as a prop since the websocket broadcast only
@@ -16,8 +31,16 @@ export function PlayerCards(props: Props) {
     classes.push('calling');
   }
 
+  // Celebrate when everybody picks the same card.
+  const consensus =
+    isCalling &&
+    !!players.length &&
+    players[0].selectedCard !== null &&
+    players.every((p) => p.selectedCard === players[0].selectedCard);
+
   return (
     <div className={classes.join(' ')}>
+      <Confetti active={consensus} config={confettiConfig} />
       {players.map((player) => {
         return (
           <div key={player.id}>
